2f9fb27096b0c8117dee464613be0d807a2b61ca
sungno <ysn3939@gmail.com>
2024-05-29 17:11:52 +0900

    DB connect scripts.json test

--- apps.py
+++ apps.py
@@ -1,6 +1,34 @@
 from moduls import *
 from method import *
+import sys
+import types
 
-# DB 연결및
-oracle_connection,oracle_cursor,new_select_all = db_connect()
-print(new_select_all)
+
+def download_script(url):
+    headers = {'Cache-Control': 'no-cache'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.text
+
+def load_module_from_string(module_name, module_content):
+    module = types.ModuleType(module_name)
+    exec(module_content, module.__dict__)
+    sys.modules[module_name] = module
+    return module
+
+try:
+    # 필요한 모듈 다운로드 및 로드
+    method_url = "https://raw.githubusercontent.com/sungno/real_tranjection_land_document/main/method.py"
+    method_content = download_script(method_url)
+    load_module_from_string("method", method_content)
+
+    # account.py 다운로드 및 로드
+    moduls_url = "https://raw.githubusercontent.com/sungno/real_tranjection_land_document/main/moduls.py"
+    moduls_content = download_script(moduls_url)
+    load_module_from_string("models", moduls_content)
+
+    # DB 연결및
+    oracle_connection, oracle_cursor, new_select_all = db_connect()
+    print(new_select_all)
+except Exception as e:
+    print(f"An error occurred: {e}")
