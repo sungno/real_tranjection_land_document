121240e10b8cffe4c151ca132d100ba2b860ae3f
sungno <ysn3939@gmail.com>
2025-09-15 11:50:54 +0900

    account del (kjc4197)

--- account.py
+++ account.py
@@ -1,6 +1,11 @@
-id_list = ['kjc4197', 'kimdabin1001', 'kjc4197']
+id_list = ['kimdabin1001']
 pw_dict = {
-    'kjc4197' : 'ja0726**!',
-    'kimdabin1001' : 'Bu0320##**',
-    'kjc4197' : 'ja0726**!'
+    'kimdabin1001' : 'Bu0320##**'
 }
+
+
+# pw_dict = {
+#     'kjc4197' : 'ja0726**!',
+#     'kimdabin1001' : 'Bu0320##**',
+#     'kjc4197' : 'ja0726**!'
+# }
